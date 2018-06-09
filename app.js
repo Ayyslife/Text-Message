c9185ed2bbcac92ad5c16e276b6b97ad991548a5
AsimAM <asim.a.m@outlook.com>
2018-06-09 13:50:34 +0100

    Completed

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+//init nexmo
+const nexmo = new Nexmo({
+    apiKey: 'your api key',
+    apiSecret: 'your api secret'
+}, {debug: true});
+
+//init app
+const app = express();
+
+//template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+//public folder setup
+app.use(express.static(__dirname + '/public'));
+
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//index route
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+//POST
+app.post('/',(req,res) => {
+    // res.send(req.body);
+    // console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        'your virtual number', number, text, {type: 'unicode'},
+        (err, responseData) =>{
+            if(err){
+                console.log(err);
+            }else{
+                console.dir(responseData);
+                //get data from response
+                const data = {
+                    id: responseData.messages[0]['message-id'],
+                    number: responseData.messages[0]['to']
+                }
+                //emit to the client
+                io.emit('smsStatus', data);
+            }
+        }
+    );
+});
+
+//port
+const port = 3000;
+
+//start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.on('dissconnect', () =>{
+        console.log('Disconnected');
+    });
+})
